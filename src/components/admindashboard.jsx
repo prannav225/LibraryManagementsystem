6fec73a04c5deec02b637b88dafb30a0835113d9
pranav225 <nagapranav.m@gmail.com>
2023-01-12 01:01:15 +0530

    added dashboard for admin and user

--- src/components/admindashboard.jsx
+++ src/components/admindashboard.jsx
@@ -1,9 +1,29 @@
+import { useEffect, useState } from 'react';
+import '../styles/admindashboard.css'
 const Admindashboard = () => {
-    return ( 
-            <div className="dashboard">
-            <h1>Dashboard</h1>
-            </div>
-     );
+    let [dashboard, setDashboard] = useState([])
+    useEffect(() => {
+        let fetchdata = async () => {
+            let response = await fetch('http://localhost:2000/dashboard')
+            let data = await response.json();
+            setDashboard(data);
+        }
+        fetchdata();
+    }, [])
+
+    return (<div className="page">
+        <h1>Admin Dashboard</h1>
+        {
+            dashboard.map(data => (
+                <div id="contanier" className="dashboard_contanier">
+                    <h1>{data.heading}</h1>
+                    <p>{data.description}</p>
+                </div>
+            ))
+        }
+
+    </div>
+    );
 }
- 
+
 export default Admindashboard;
--- src/components/userDashboard.jsx
+++ src/components/userDashboard.jsx
@@ -1,9 +1,27 @@
+import '../styles/userdashboard.css'
+import { useState, useEffect } from "react"
 const UserDashboard = () => {
-    return ( 
+    let [dashboard, setDashboard] = useState([])
+    useEffect(() => {
+        let fetchdata = async () => {
+            let response = await fetch('http://localhost:2000/dashboard')
+            let data = await response.json();
+            setDashboard(data);
+        }
+        fetchdata();
+    }, [])
+    return (
         <div className="userDashboard">
             <h1>Welcome to Our Library Management System</h1>
+            {dashboard.map(data => (
+                <div id="contanier" className="dashboard_contanier">
+                    <h1>{data.heading}</h1>
+                    <p>{data.description}</p>
+                </div>
+            ))}
         </div>
-     );
+
+    );
 }
- 
+
 export default UserDashboard;
